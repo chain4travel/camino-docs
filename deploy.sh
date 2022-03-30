bde57b1396b8fcc053a068ea4c35a50080a222aa
c4t <c4t@videvago.com>
2022-03-30 11:28:14 +0000

    Added error handling to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+function CRIT {
+	echo "Failed step: $1"
+	exit 1
+}
+
+
 if [ -z "${TYPESENSE_API_KEY}" ] ; then
 	echo "TYPESENSE_API_KEY must be set before executing this script!"
 	exit 1
 fi
 
-yarn
-yarn build
-ssh web@192.168.220.8 rm -rf /var/www/docs.camino.foundation/html/*
-scp -r build/* web@192.168.220.8:/var/www/docs.camino.foundation/html/
+yarn || CRIT "yarn (prepare)"
+yarn build || CRIT "build of the documentation"
+ssh web@192.168.220.8 rm -rf /var/www/docs.camino.foundation/html/* || CRIT "removing old html files"
+scp -r build/* web@192.168.220.8:/var/www/docs.camino.foundation/html/ || CRIT "deploy to server"
 
 cd scraper
-./execute_scraper.sh
+./execute_scraper.sh || CRIT "renew scrapper"
